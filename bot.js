d9a5abf24f5e3da005d68f746c702915227de17e
Curtis Schofield <github@robotarmyma.de>
2011-01-05 15:37:48 -0800

    multi-server

--- bot.js
+++ bot.js
@@ -13,43 +13,39 @@ var error_f = function(err) {
     throw err
 }
 
-if(config.ircbot() == true) {
-	var bot = new irc.Client(config.server(), config.nick(), {
-		channels: config.channels(),
-	})
+  config.servers(function(config) {
+    var bot = new irc.Client(config.server, config.nick, {
+      channels: config.channels,
+    })
+    function authbot() {
+      bot.say('nickserv', ("identify "+config.password))
+      console.log("AUTH SENT.")
+    }
 
-	// load welcome message
-	console.log('tocho is getting ready to listen & log...')
+    console.log('tocho is getting ready to listen & log...')
 
-	// temp auth 
-	var auth = config.auth()
-  //=== !last
-	bot.addListener('message', function (from, to, message) {
-    json_message = {date:Date(), from:from, message:message }
-    var bang = BangLast.create(red.channel(to))
-    SpeakTo.bang_speaker(bot,bang)(from,message)
+    bot.addListener('message', function (from, to, message) {
+      json_message = {date:Date(), from:from, message:message }
+      var bang = BangLast.create(red.channel(to))
+      SpeakTo.bang_speaker(bot,bang)(from,message)
+    })
+
+    bot.addListener('message', function (from, to, message) {
+      json_message = {date:Date(), from:from, message:message }
+      red.channel(to).record(JSON.stringify(json_message))
+    })
+
+    bot.addListener('pm', function (from, message) {
+      json_message = {date:Date(), from:from, message:message }
+      red.channel(from).record(JSON.stringify(json_message))
+    })
+
+
+    bot.addListener('pm', function (from, message) {
+      console.log(from + ' => TOCHO: ' + message)
+    })
+
+    if(config.auth)
+      setTimeout(authbot, 20000)
+
   })
-
-	bot.addListener('message', function (from, to, message) {
-    json_message = {date:Date(), from:from, message:message }
-    red.channel(to).record(JSON.stringify(json_message))
-  })
-
-	bot.addListener('pm', function (from, message) {
-    json_message = {date:Date(), from:from, message:message }
-    red.channel(from).record(JSON.stringify(json_message))
-  })
-
-
-	bot.addListener('pm', function (from, message) {
-	    console.log(from + ' => TOCHO: ' + message)
-	})
-
-	if(auth == true)
-		setTimeout(authbot, 20000)
-
-	function authbot() {
-		bot.say('nickserv', ("identify "+config.password()))
-		console.log("AUTH SENT.")
-	}
-}
